b160dd2f94d6ceaa83bb117e88190b526f1c2c16
Aleksander <asmirnov76@rambler.ru>
2022-09-08 03:23:34 +0300

    закоментировал последнюю задачу

--- Home_work001/Program.cs
+++ Home_work001/Program.cs
@@ -49,7 +49,7 @@ else
 */
 
 //Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-
+/*
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 int count = 1;
@@ -62,3 +62,4 @@ while (count <= number)
     }
     count += 1;
 }
+*/
